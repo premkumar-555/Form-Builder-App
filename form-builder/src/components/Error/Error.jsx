2d88b1a1f0559c4c727b561375cd2beb40802e2a
Premkumar-hulikoppe <premuhulikoppe@gmail.com>
2023-01-12 15:11:46 +0530

    created routes for pages

---
+++ form-builder/src/components/Error/Error.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Error = () => {
+  return (
+    <div>
+      <h1>Error: 404</h1>
+      <h1>Requested Page is Not Found!</h1>
+    </div>
+  );
+};
+
+export default Error;
---
+++ form-builder/src/components/Routes/Pages.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../Home/Home";
+import NavBar from "../NavBar/NavBar";
+import CreateForm from "../CreateForm/CreateForm";
+import Error from "../Error/Error";
+const Pages = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="create_form" element={<CreateForm />} />
+      </Routes>
+    </>
+  );
+};
+
+export default Pages;
